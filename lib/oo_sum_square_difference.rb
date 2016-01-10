6b7c37d27147acd8d8da1e1a9aa3a1fa19acb1ce
parora86 <parora.cpa@outlook.com>
2016-01-10 05:53:34 +0000

    Done.

--- lib/oo_sum_square_difference.rb
+++ lib/oo_sum_square_difference.rb
@@ -1,1 +1,24 @@
-# Implement your object-oriented solution here!
+class SumSquareDifference
+  def initialize(number_int)
+    @number = number_int
+  end
+  
+  def difference()
+    counter = 1
+    sum1 = 0
+    @number.times do
+      sum1 += counter**2
+      counter += 1
+    end
+
+    counter = 1
+    sum2 = 0
+    @number.times do
+      sum2 += counter
+      counter += 1
+    end
+    sum2 = sum2**2
+    return (sum2-sum1)
+
+  end
+end
--- lib/sum_square_difference.rb
+++ lib/sum_square_difference.rb
@@ -1,1 +1,20 @@
-# Implement your procedural solution here!
+require 'pry'
+
+def sum_square_difference(number)
+  counter = 1
+  sum1 = 0
+  number.times do
+    sum1 += counter**2
+    counter += 1
+  end
+
+  counter = 1
+  sum2 = 0
+  number.times do
+    sum2 += counter
+    counter += 1
+  end
+  sum2 = sum2**2
+  return (sum2-sum1)
+
+end
